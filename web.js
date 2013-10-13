3c498f5ff88f38ddfa8b9dc5170650d6fd69d9e1
unknown <myemailum14@gmail.com>
2013-10-13 11:27:15 -0700

    'hellow'

--- web.js
+++ web.js
@@ -4,11 +4,7 @@ var exec = require('child_process').exec;
 
 http.createServer(function(req, res) {
 var abc = url.parse(req.url, true), path = abc.pathname;
-if (path=='/90183692268807934844204653499881516397615934124226775735065875857624799287860312769887154161311253979823223859244357789554574701001496584526468171041383/gitit'){
-	exec('git remote add origin https://github.com/techsin/cargames.git',function(){});
-	exec('git pull',function(){});
-	return;
-}
+
 fs.readFile('./index.html', function(err, html) {
   	if (err) { throw err; }
   	res.writeHead(200, {"Content-Type": "text/html"});
