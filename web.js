766055fcd1a24d4d878df676f91730cbc3e70ebd
unknown <myemailum14@gmail.com>
2013-10-13 11:22:18 -0700

    'working'

--- web.js
+++ web.js
@@ -6,16 +6,15 @@ http.createServer(function(req, res) {
 var abc = url.parse(req.url, true), path = abc.pathname;
 var port = process.env.PORT || 5000;
 if (path=='/90183692268807934844204653499881516397615934124226775735065875857624799287860312769887154161311253979823223859244357789554574701001496584526468171041383/gitit'){
-	exec('git remote add origin https://github.com/techsin/cargame.git',function(){});
+	exec('git remote add origin https://github.com/techsin/cargames.git',function(){});
 	exec('git pull',function(){});
 	return;
 }
 fs.readFile('./index.html', function(err, html) {
-  	if (err) { throw err; } else {
+  	if (err) { throw err; }
   	res.writeHead(200, {"Content-Type": "text/html"});
   	res.write(html);
 	  res.end();
-	}
 });
 
 }).listen(port);
