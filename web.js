8cbdeae11974cb14e84cd3826a29d9d4aa3d2c53
unknown <myemailum14@gmail.com>
2013-10-13 10:49:14 -0700

    'hellow'

---
+++ web.js
@@ -0,0 +1,18 @@
+var http = require('http'), fs = require('fs'), url = require('url'), nodemailer = require("nodemailer");
+var exec = require('child_process').exec;
+
+
+http.createServer(function(req, res) {
+if (path=='/90183692268807934844204653499881516397615934124226775735065875857624799287860312769887154161311253979823223859244357789554574701001496584526468171041383/gitit'){
+	exec('git remote add origin https://github.com/techsin/cargame.git',function(){});
+	exec('git pull',function(){});
+}
+fs.readFile('./index.html', function(err, html) {
+     if (err) { throw err; } else {
+  var abc = url.parse(req.url, true), path = abc.pathname;
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write(html);
+  res.end();
+}
+
+}}).listen(80);
